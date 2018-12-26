967d00f1f8e368165faed7d51e917d3f947ccfc5
sachin <ssjadhav415@gmail.com>
2018-12-26 12:39:01 +0530

    cloud update changs:sj

--- cloudServerUpdate.sh
+++ cloudServerUpdate.sh
@@ -1,25 +1,18 @@
 rm -rf ../PG_Cloud_Server 
 rm -rf ../PGServerJs.tar.gz
-
 git pull
 echo "git pull done"
-
-cp $1/PGServerJs.tar.gz ../
-
-
-tar -xvf ../PGServerJs.tar.gz
+cp ./$1/PGServerJs.tar.gz ../
+cd ..
+tar -xvf PGServerJs.tar.gz
 echo "extract done"
-
-mv ../PGServerJs ../PGServerJs_`date +"%Y-%m-%d"`
+mv ./PGServerJs ./PGServerJs_`date +"%Y-%m-%d"`
 echo "PGServer folder moved"
-
-mv ../PG_Cloud_Server/PGServerJs ../
-cd ../PGServerJs
-
+mv ./PG_Cloud_Server/PGServerJs ./
+cd PGServerJs
 echo "npm install start"
 npm install
 echo "npm install done"
-
 node startServerCloud.js --app $1
 pm2 status
 pm2 logs
